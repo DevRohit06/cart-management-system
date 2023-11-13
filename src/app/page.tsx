eb9ef47f2a2be77b97c4855f674192fd28b370c2
Rohit Kushwaha <technicalrohit06@gmail.com>
2023-11-13 19:00:36 +0530

    added loaders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
-"use client"
-import Banner from '@/components/announcement'
-import Image from 'next/image'
-import NavBar from '@/components/navBar'
-import ProductList from '@/components/productList'
-import useAxios from '@/hook/useAxios'
+"use client";
+import Banner from "@/components/announcement";
+import Image from "next/image";
+import NavBar from "@/components/navBar";
+import ProductList from "@/components/productList";
+import useAxios from "@/hook/useAxios";
 export default function Home() {
   const { response } = useAxios("products");
-  if(!response) return <div>Loading...</div>
+  function loading() {
+    if (!response) {
+      return (
+        <>
+          <div className="flex  justify-center items-center  mt-64 w-[100%]">
+            <div className="loader"></div>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Banner />
+          <NavBar />
+          <ProductList />
+        </>
+      );
+    }
+  }
   return (
-    <main>
-      <Banner/>
-      <NavBar/>
-      <ProductList/>
-    </main>
-  )
+    <>
+      <main className="relative w-full h-full">
+        {loading()}
+      </main>
+    </>
+  );
 }
--- src/components/productList.tsx
+++ src/components/productList.tsx
@@ -6,80 +6,96 @@ export default function ProductList() {
   const cartProducts = useSelector((state: any) => state.allCart.cart);
   const dispatch = useDispatch();
   const { response } = useAxios("products");
-  if (!response) {
-    return <div>Loading...</div>;
-  }
+
   const products: [] = response || [];
+  function loading() {
+    if (!response) {
+      return (
+        <>
+            <div className="w-full h-96 flex justify-center items-center  ">
+              <div className="loader"></div>
+            </div>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div className="relative bg-white">
+            <div className="mx-auto max-w-2xl px-4 pb-16 pt-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+              <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+                Customers also purchased
+              </h2>
 
-  return (
-    <>
-      <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 pb-16 pt-8 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Customers also purchased
-          </h2>
-
-          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 h-full">
-            {products.map((product: any, index: number) => {
-              function checkProduct() {
-                let result = cartProducts.some((item: any) => {
-                  return item.id === product.id;
-                });
-                if (result) {
+              <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 h-full">
+                {products.map((product: any, index: number) => {
+                  function checkProduct() {
+                    let result = cartProducts.some((item: any) => {
+                      return item.id === product.id;
+                    });
+                    if (result) {
+                      return (
+                        <button
+                          disabled
+                          className="w-full mt-1 py-2 text-center font-semibold text-white bg-gray-900/80 rounded-xl"
+                        >
+                          <h1>Added to cart</h1>
+                        </button>
+                      );
+                    } else {
+                      return (
+                        <button
+                          onClick={() =>
+                            dispatch(addToCart({ ...product, quantity: 1 }))
+                          }
+                          className="w-full mt-1 py-2 text-center font-semibold text-white bg-gray-900 rounded-xl"
+                        >
+                          <h1>Add to cart</h1>
+                        </button>
+                      );
+                    }
+                  }
                   return (
-                    <button
-                      disabled
-                      className="w-full mt-1 py-2 text-center font-semibold text-white bg-gray-900/80 rounded-xl"
-                    >
-                      <h1>Added to cart</h1>
-                    </button>
-                  );
-                } else {
-                  return (
-                    <button
-                      onClick={() => dispatch(addToCart({...product, quantity: 1}))}
-                      className="w-full mt-1 py-2 text-center font-semibold text-white bg-gray-900 rounded-xl"
-                    >
-                      <h1>Add to cart</h1>
-                    </button>
-                  );
-                }
-              }
-              return (
-                <div key={index} className=" flex flex-col justify-between">
-                  <div key={product.id} className=" relative ">
-                    <div className="w-auto flex justify-center overflow-hidden rounded-md  h-56">
-                      <img
-                        src={product.image}
-                        alt={product.title}
-                        className="h-full "
-                      />
-                    </div>
-                    <div className="mt-4 justify-between">
-                      <div>
-                        <h3 className="text-sm text-gray-700">
-                          <a href="">
-                            <span
-                              aria-hidden="true"
-                              className="absolute inset-0"
-                            />
-                            {product.title}
-                          </a>
-                        </h3>
+                    <div key={index} className=" flex flex-col justify-between">
+                      <div key={product.id} className=" relative ">
+                        <div className="w-auto flex justify-center overflow-hidden rounded-md  h-56">
+                          <img
+                            src={product.image}
+                            alt={product.title}
+                            className="h-full "
+                          />
+                        </div>
+                        <div className="mt-4 justify-between">
+                          <div>
+                            <h3 className="text-sm text-gray-700">
+                              <a href="">
+                                <span
+                                  aria-hidden="true"
+                                  className="absolute inset-0"
+                                />
+                                {product.title}
+                              </a>
+                            </h3>
+                          </div>
+                          <p className="text-xl  font-medium text-gray-900">
+                            ₹ {product.price}
+                          </p>
+                        </div>
                       </div>
-                      <p className="text-xl  font-medium text-gray-900">
-                        ₹ {product.price}
-                      </p>
-                    </div>
-                  </div>
 
-                  {checkProduct()}
-                </div>
-              );
-            })}
+                      {checkProduct()}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+      );
+    }
+  }
+  return <>
+  <div>
+    {loading()}
+  </div>
+  </>;
 }
